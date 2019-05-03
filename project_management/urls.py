fa04d315fa35f40b57925324850f8598953a3f49
david.kaggulire <dkaggulire@gmail.com>
2019-05-03 17:58:18 +0300

    Adds urls for project team members

--- project_management/urls.py
+++ project_management/urls.py
@@ -64,10 +64,12 @@ urlpatterns = [
     path('validateProjectTeamName/', views.validateProjectTeamName, name='validateProjectTeamName'),
     path('validateProjectAssigned/', views.validateProjectAssigned, name='validateProjectAssigned'),
 
-    path('addProjectMember/', views.AddProjectMember.as_view(), name='addProjectMember'),
-    path('listProjectMembers/', views.ListProjectMembers.as_view(), name='listProjectMembers'),
-    path('deleteProjectMember/<int:pk>', views.DeleteProjectMember.as_view(), name='deleteProjectMember'),
-    path('validateProjectMember/', views.validateProjectMember, name='validateProjectMember'),
+    path('addProjectTeamMember/', views.AddProjectTeamMember.as_view(), name='addProjectTeamMember'),
+    path('listProjectTeamMembers/', views.ListProjectTeamMembers.as_view(), name='listProjectTeamMembers'),
+    path('updateProjectTeamMember/<int:pk>', views.UpdateProjectTeamMember.as_view(), name='updateProjectTeamMember'),
+    path('detailProjectTeamMembers/', views.detail_team_member, name='detailProjectTeamMembers'),
+    path('deleteProjectTeamMember/', views.remove_project_team_member, name='deleteProjectTeamMember'),
+    path('validateProjectTeamAssigned/', views.validateProjectTeamAssigned, name='validateProjectTeamAssigned'),
 
     path('listAllRoles/', views.ListAllRoles.as_view(), name='listAllRoles'),
     path('addRole/', views.AddRole.as_view(), name='addRole'),
