6c070e108915f1bda2ad98e13577e2c7c8aa9a04
david.kaggulire <dkaggulire@gmail.com>
2020-07-22 16:37:51 +0300

    Edit cc in email

--- project_management/views.py
+++ project_management/views.py
@@ -8815,11 +8815,11 @@ def export_email_timesheet_task_report(request):
 
     msg = render_to_string('project_management/email_template_timesheet_report.html', context22)
 
-    email_address = ['gigi@sybyl.com', 'babirye.grace@sybyl.com', 'ampumuza.amon@sybyl.com', 'jerry.vijayan@sybyl.com', 'chepkurui.job@sybyl.com', 'wamboza.allan@sybyl.com', 'david.kaggulire@sybyl.com', 'jeremiah.kerman@sybyl.com', 'sajin.mathew@sybyl.com']
+    email_address = ['babirye.grace@sybyl.com', 'ampumuza.amon@sybyl.com', 'jerry.vijayan@sybyl.com', 'chepkurui.job@sybyl.com', 'wamboza.allan@sybyl.com', 'david.kaggulire@sybyl.com', 'jeremiah.kerman@sybyl.com', 'sajin.mathew@sybyl.com']
     subject, from_email, to = 'SYBYL', 'from@example.com', email_address
     text_content = 'SERVICE DESK.'
     html_content = msg
-    msg = EmailMultiAlternatives(subject, text_content, from_email, cc=email_address)
+    msg = EmailMultiAlternatives(subject, text_content, from_email, to=['gigi@sybyl.com'], cc=email_address)
     msg.attach('TimesheetReport.xls', excelfile.getvalue(), 'application/ms-excel')
     msg.attach_alternative(html_content, "text/html")
     msg.send()
